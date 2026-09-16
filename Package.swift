ebd5f7cc3f74b875481ef5a352be8b0ce63e2a94
Oktay Tanrıkulu <oktaytanrikulu@gmail.com>
2026-09-17 00:20:49 +0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CustomRouting",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CustomRouting",
+            targets: ["CustomRouting"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "CustomRouting",
+            swiftSettings: [
+                .enableUpcomingFeature("ApproachableConcurrency"),
+            ],
+        ),
+        .testTarget(
+            name: "CustomRoutingTests",
+            dependencies: ["CustomRouting"],
+            swiftSettings: [
+                .enableUpcomingFeature("ApproachableConcurrency"),
+            ],
+        ),
+    ]
+)
